4c91a6cc5655dd72d44f07cf0de8f16e016fa843
Anthony DeRossi <ajderossi@gmail.com>
2020-11-12 01:05:45 -0800

    Check for a compatible system on init

--- asus-ec-sensors.c
+++ asus-ec-sensors.c
@@ -5,6 +5,7 @@
  * Copyright (C) 2020 Anthony DeRossi <ajderossi@gmail.com>
  */
 
+#include <linux/dmi.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
@@ -13,6 +14,18 @@ MODULE_AUTHOR("Anthony DeRossi <ajderossi@gmail.com>");
 MODULE_DESCRIPTION("Asus EC sensor driver");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.1");
+
+static const struct dmi_system_id asus_ec_sensors_dmi_match[] = {
+	{
+		/* Asus Prime X570-Pro */
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "ASUSTeK COMPUTER INC."),
+			DMI_MATCH(DMI_BOARD_NAME, "PRIME X570-PRO"),
+		},
+	},
+	{}
+};
+MODULE_DEVICE_TABLE(dmi, asus_ec_sensors_dmi_match);
 
 static struct platform_driver asus_platform_driver = {
 	.driver = {
@@ -29,6 +42,9 @@ static struct platform_device *asus_platform_device;
 
 static int __init asus_ec_sensors_init(void)
 {
+	if (!dmi_first_match(asus_ec_sensors_dmi_match))
+		return -ENODEV;
+
 	asus_platform_device = platform_create_bundle(&asus_platform_driver,
 						      asus_platform_probe,
 						      NULL, 0, NULL, 0);
